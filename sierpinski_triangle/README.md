158cfb5b968e4703d33dd69368153768d409d7fb
Reyniel Mark Escamillas <rmescamillas26@gmail.com>
2023-01-26 15:22:26 +0800

    Done version 1

--- sierpinski_triangle/README.md
+++ sierpinski_triangle/README.md
@@ -2,6 +2,8 @@
 
 - Visualize the Sierpinski Triangle
 
+I just see this in TikTok and I want to try it. So that I research, and created a desktop app that will emulate Sierpinski Triangle.
+
 The Chaos Game
 
 1. Take three points in a plane to form a triangle.
@@ -15,3 +17,15 @@ The Chaos Game
 - [Chaos Game](https://www.pythoninformer.com/generative-art/iterated-function-systems/chaos-game/)
 - [How To Determine if a Point Is in a 2D Triangle](https://www.baeldung.com/cs/check-if-point-is-in-2d-triangle)
 - [Applying the midpoint formula to find the midpoint between two points](https://youtu.be/6mx8HIf3oUk?t=74)
+
+## Create Installer
+Pre-condition: Must be inside of sierpinski_triangle module before running this
+1. Run PyInstaller
+- Option 1: Create `one file` exe ( Suggested)
+    ```
+    pyinstaller.exe --add-data "*.ico;."  --add-data "static/images/*.png;./static/images"  --add-data "static/images/*.jpg;./static/images"  --add-data "static/ui/*.ui;./static/ui" --onefile --windowed --icon=app.ico app.py
+    ```
+- Option 2:
+    ```
+    pyinstaller.exe --add-data "*.ico;."  --add-data "static/images/*.png;./static/images"  --add-data "static/images/*.jpg;./static/images"  --add-data "static/ui/*.ui;./static/ui" --windowed --icon=app.ico app.py
+    ```
--- sierpinski_triangle/app.py
+++ sierpinski_triangle/app.py
@@ -1,11 +1,14 @@
 import sys
+import os
 
 import pyqtgraph as pg
 from path_config import static_image_path, static_ui_path
 from PyQt6 import uic
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from sierpinski import SierpinskiTriangle
 
+VERSION = "v1.1"
 
 class SierpinskiTriangleWindow(QMainWindow,SierpinskiTriangle):
 
@@ -17,11 +20,25 @@ class SierpinskiTriangleWindow(QMainWindow,SierpinskiTriangle):
 
         self.setWindowTitle("Sierpinski Triangle Emulator")
 
+        iconpath = static_image_path("Sierpinski_Triangle_Logo-nobg.png")
+        if os.path.exists(iconpath):
+            self.setWindowIcon(QIcon(iconpath))
+
+        self.center_form()
+
+        self.version_label.setText(VERSION)
+
         self.graph_widget.setBackground('w')
         self.graph_widget.showGrid(x=True,y=True)
 
         self.step_at.setText("0")
         self.step_all.setText("0")
+
+        self.action_reset.triggered.connect(self.reset)
+        self.action_generate_all.triggered.connect(self.generate_all)
+        self.action_run_chaos.triggered.connect(self.run_chaos_game)
+        self.action_export.triggered.connect(self.export_points)
+        self.action_import.triggered.connect(self.import_points)
 
         self.generate_triangle_btn.clicked.connect(self.generate_valid_triangle)
         self.generate_starting_point_btn.clicked.connect(self.generate_random_point)
@@ -30,14 +47,34 @@ class SierpinskiTriangleWindow(QMainWindow,SierpinskiTriangle):
 
         self.run_chaos_btn.clicked.connect(self.run_chaos_game)
 
+        self.prev_btn.clicked.connect(self.previous)
+        self.next_btn.clicked.connect(self.next)
 
+
+    def center_form(self):
+        qr = self.frameGeometry()
+        cp = self.screen().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
     def set_range(self):
         self.range = self.scale_num.value()
 
     def reset(self):
-        self.scale_num.setValue(1)
+        self.scale_num.setValue(100)
         self.generate_valid_triangle()
+        self.point_s_x.setValue(0)
+        self.point_s_y.setValue(0)
+        self.num_steps.setValue(0)
+        self.status_browser.clear()
+
+    def export_points(self):
+        self.status_browser.clear()
+        self.status_browser.append("Export Points Button Feature not enabled yet")
+
+    def import_points(self):
+        self.status_browser.clear()
+        self.status_browser.append("Import Points Button Feature not enabled yet")
 
     def generate_valid_triangle(self):
         self.set_range()
@@ -65,7 +102,7 @@ class SierpinskiTriangleWindow(QMainWindow,SierpinskiTriangle):
         color ={
             "triangle":pg.mkBrush(255, 0, 50),
             "start":pg.mkBrush(255, 139, 19),
-            "points":pg.mkBrush(32, 82, 149, 120)
+            "points":pg.mkBrush(33, 150, 243, 120)
         }
         scatter = pg.ScatterPlotItem(size=10, brush=color[point_color])
         scatter.addPoints(x_data, y_data)
@@ -104,9 +141,13 @@ class SierpinskiTriangleWindow(QMainWindow,SierpinskiTriangle):
                 f"The Starting point {starting_point} is not inside of the triangle"
                 )
 
+    def previous(self):
+        self.status_browser.clear()
+        self.status_browser.append("Previous Button Feature not enabled yet")
 
-
-
+    def next(self):
+        self.status_browser.clear()
+        self.status_browser.append("Next Button Feature not enabled yet")
 
 app = QApplication(sys.argv)
 
